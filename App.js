5a0dc20fddf1dcd5b4121bc61a504e48c3644715
Zhi Xin <23009854@myrp.edu.sg>
2025-02-05 11:09:41 +0800

    Exercises

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import { StatusBar, Button, StyleSheet, View } from 'react-native';
+import { Audio } from 'expo-av';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
+
+export default function App() {
+    const [mySound, setMySound] = useState(null);
+
+    async function playSound() {
+        try {
+            if (mySound) {
+                await mySound.unloadAsync();
+            }
+            const { sound } = await Audio.Sound.createAsync(require('./assets/short1.wav'));
+            setMySound(sound);
+            await sound.playAsync();
+        } catch (error) {}
+    }
+
+    useEffect(() => {
+        return mySound ? () => mySound.unloadAsync() : undefined;
+    }, [mySound]);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar />
+            <Button title="Play Sound" onPress={playSound} />
+        </View>
+    );
+}
